3cc2bd1446d997ea7e78d2b4edbc264be2d6500a
mainyaa <kazuyuki.mori@topgate.ne.jp>
2013-11-26 17:12:08 +0900

    Fixed test config

--- karma.conf.js
+++ karma.conf.js
@@ -1,61 +1,52 @@
-'use strict';
+// Karma configuration
+// http://karma-runner.github.io/0.10/config/configuration-file.html
 
-module.exports = function (config) {
-    config.set({
-        // Karma configuration
+module.exports = function(config) {
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-        // base path, that will be used to resolve files and exclude
-        basePath: '',
-        frameworks: ['jasmine'],
+    // testing framework to use (jasmine/mocha/qunit/...)
+    frameworks: ['jasmine'],
 
-        // list of files / patterns to load in the browser
-        files: [
-          'app/bower_components/angular/angular.js',
-          'app/bower_components/angular-mocks/angular-mocks.js',
-          'app/scripts/*.js',
-          'app/scripts/**/*.js',
-          'test/mock/**/*.js',
-          'test/spec/**/*.js'
-        ],
+    // list of files / patterns to load in the browser
+    files: [
+      'app/bower_components/angular/angular.js',
+      'app/bower_components/angular/angular-mocks.js',
+      'app/scripts/*.coffee',
+      'app/scripts/**/*.coffee',
+      'test/mock/**/*.coffee',
+      'test/spec/**/*.coffee'
+    ],
 
-        // list of files to exclude
-        exclude: [],
+    // list of files / patterns to exclude
+    exclude: [],
 
-        // test results reporter to use
-        // possible values: dots || progress || growl
-        reporters: ['progress'],
+    // web server port
+    port: 8080,
 
-        // web server port
-        port: 8080,
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
-        // cli runner port
-        runnerPort: 9100,
 
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
-        // level of logging
-        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-        logLevel: config.LOG_INFO,
 
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: false,
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
 
-        // Start these browsers, currently available:
-        // - Chrome
-        // - ChromeCanary
-        // - Firefox
-        // - Opera
-        // - Safari (only Mac)
-        // - PhantomJS
-        // - IE (only Windows)
-        browsers: ['Chrome'],
 
-        // If browser does not capture in given timeout [ms], kill it
-        captureTimeout: 5000,
-
-        // Continuous Integration mode
-        // if true, it capture browsers, run tests and exit
-        singleRun: false
-    });
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
 };
